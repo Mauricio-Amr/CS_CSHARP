711a752eff70e9761a7f0ce8446a4137f0c5fc95
Mauricio-Amr <75991797+Mauricio-Amr@users.noreply.github.com>
2022-06-28 13:02:53 -0300

    Aula herança finalizada
    aula sobre herança finalizada

--- heranca1/heranca1/Program.cs
+++ heranca1/heranca1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using heranca1.Entities;
 
 namespace heranca1
 {
@@ -10,6 +11,17 @@ namespace heranca1
     {
         static void Main(string[] args)
         {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Holder);
+            Console.WriteLine(account.Balance);
+
+
+            account.Deposit(200.0);
+            Console.WriteLine(account.Balance);
+
+
+
         }
     }
 }
